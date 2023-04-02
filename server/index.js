defef333559a55359157caecd983e5ca185074db
Jana <janakrish3011@gmail.com>
2023-04-02 12:28:02 +0530

    MongoDB connection is established

--- server/index.js
+++ server/index.js
@@ -1,4 +1,19 @@
-import bodyParser from "body-parser";
-import bodyParser from "body-parser";
+import bodyParser from 'body-parser';
 import cors from 'cors';
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
+import express from 'express'
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://legeon:legeon3011@cluster0.qclaglu.mongodb.net/?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
