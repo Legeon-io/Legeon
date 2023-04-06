b584d2181d6c2cf766ef39d4d8f66d5a8049d955
Jana <janakrish3011@gmail.com>
2023-04-06 22:42:15 +0530

    Added pages and components folders in src

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,22 @@
 import React from "react";
 
+import {
+    Home,
+    Bookings,
+    Community,
+    Engage,
+    Profile,
+} from "./pages";
+
 const App = () => {
     return (
         <div>
             <h1>Legeon</h1>
+            <Home />
+            <Bookings />
+            <Community />
+            <Engage />
+            <Profile />
         </div>
     )
 }
