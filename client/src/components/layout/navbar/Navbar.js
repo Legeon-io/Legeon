3eca433055b58a498e43f3b78bd67903906cecdf
Jana <janakrish3011@gmail.com>
2023-04-07 21:24:30 +0530

    Added CSS to sidebar and also included routes in App.js

--- client/src/components/layout/navbar/Navbar.js
+++ client/src/components/layout/navbar/Navbar.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react'
 import * as AiIcons from 'react-icons/ai';
+import * as FaIcons from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
 import { Sidebar } from "../sider/Sidebar.js";
+import './Navbar.css';
+import { IconContext } from 'react-icons';
 
 export const Navbar = () => {
   const [ sidebar, setSidebar ] = useState(false)
@@ -10,13 +13,14 @@ export const Navbar = () => {
   const showSidebar = () => setSidebar(!sidebar)
   return (
     <>
+    <IconContext.Provider value={{color: '#fff'}}>
       <div className='navbar'>
         <Link to="#" className='menu-bar'>
-          <AiIcons.AiOutlineBars onClick={showSidebar}  />
+          <FaIcons.FaBars onClick={showSidebar}  />
         </Link>
       </div>
       <nav className={ sidebar ? 'nav-menu active' : 'nav-menu' }>
-        <ul className='nav-menu items'>
+        <ul className='nav-menu-items' onClick={showSidebar}>
           <li className='navbar-toggle'>
             <Link to="#" className='menu-bars'>
               <AiIcons.AiFillCloseCircle />
@@ -34,6 +38,7 @@ export const Navbar = () => {
           })}
         </ul>
       </nav>
+      </IconContext.Provider>
     </>
   )
 }
