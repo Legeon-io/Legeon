a7f97d9ca09294b49dfc9f30542b2082d6592e69
Jana <janakrish3011@gmail.com>
2023-05-02 22:00:29 +0530

    Added Services top container

---
+++ client/src/components/common/services_cards/ServiceDisplayCard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ServiceDisplayCard = () => {
+  return (
+    <div>ServiceDisplayCard</div>
+  )
+}
+
+export default ServiceDisplayCard
--- client/src/pages/services/Services.js
+++ client/src/pages/services/Services.js
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Services.css';
+import '../index.css'
 
-export const Services = ({sidebarVisible}) => {
+export const Services = ({ sidebarVisible }) => {
+  const [virtualEngage, setVirtualEngage] = useState(false);
 
   return (
-    <div className={sidebarVisible ? 'services move-right' : 'services'} >
-      Services
-    </div>
+    <>
+      <div className={sidebarVisible ? 'services move-right' : 'services'} >
+
+        <div className='services-container'>
+          <span className='services-text'>
+            Services
+          </span>
+          <span className='add-services'>
+            <button className='services' >
+              + Add Services
+            </button>
+          </span>
+        </div>
+      </div>
+      <div className='division'></div>
+    </>
   )
 }
 
