2aaad5972f7a2144fb7a8d11fa449a6dce506c14
Jana <janakrish3011@gmail.com>
2023-05-14 11:53:01 +0530

    Added css to service form

--- client/src/components/common/services_cards/ServiceDisplayCard.js
+++ client/src/components/common/services_cards/ServiceDisplayCard.js
@@ -1,42 +1,40 @@
 import React from 'react'
 import './ServiceDisplayCard.css';
 
-const ServiceDisplayCard = (sidebarVisible) => {
+const ServiceDisplayCard = () => {
   return (
     <>
-      {/* <div className={sidebarVisible ? 'page move-right' : 'page'} > */}
 
         <div className='service_cards-container'>
-          <div class="service_card">
-            <div class="service_card-header">
-              <h2 class="service_card-title">Service Name</h2>
-              <div class="service_card-price">$100/hour</div>
+          <div className="service_card">
+            <div className="service_card-header">
+              <h2 className="service_card-title">Service Name</h2>
+              <div className="service_card-price">$100/hour</div>
             </div>
-            <div class="service_card-body">
-              <p class="service_card-description">Description of the service goes here</p>
-              <div class="service_card-call-duration">
+            <div className="service_card-body">
+              <p className="service_card-description">Description of the service goes here</p>
+              <div className="service_card-call-duration">
                 <strong>Call duration:</strong> 30 minutes
               </div>
-              <button class="edit-button">Edit</button>
+              <button className="edit-button">Edit</button>
             </div>
           </div>
 
-          <div class="service_card">
-            <div class="service_card-header">
-              <h2 class="service_card-title">Service Name</h2>
-              <div class="service_card-price">$100/hour</div>
+          <div className="service_card">
+            <div className="service_card-header">
+              <h2 className="service_card-title">Service Name</h2>
+              <div className="service_card-price">$100/hour</div>
             </div>
-            <div class="service_card-body">
-              <p class="service_card-description">Description of the service goes here</p>
-              <div class="service_card-call-duration">
+            <div className="service_card-body">
+              <p className="service_card-description">Description of the service goes here</p>
+              <div className="service_card-call-duration">
                 <strong>Call duration:</strong> 30 minutes
               </div>
-              <button class="edit-button">Edit</button>
+              <button className="edit-button">Edit</button>
             </div>
           </div>
         </div>
-
-      {/* </div> */}
+        <br />
     </>
   )
 }
--- client/src/components/common/services_cards/ServiceForm.js
+++ client/src/components/common/services_cards/ServiceForm.js
@@ -21,24 +21,40 @@ const ServiceForm = () => {
         else
             setCustomBool(false);
 
-        setDuration(parseInt(event.target.value));
+        if(event.target.value === 'custom')
+            setDuration(0)
+        else 
+            setDuration(parseInt(event.target.value));
     };
 
     const handleCustomDurationChange = (event) => {
         setDuration(parseInt(event.target.value));
+
+        if(event.target.value === '')
+            setDuration(0)
+        else 
+            setDuration(parseInt(event.target.value));
     };
     
     const handlePriceChange = (event) => {
-        if (event.target.id === 'customPrice')
-        setCustomPriceBool(true);
+        if (event.target.id === 'customPrice') 
+            setCustomPriceBool(true);
         else
-        setCustomPriceBool(false);
+            setCustomPriceBool(false);
         
-        setPrice(parseInt(event.target.value));
+        if(event.target.value === 'custom')
+            setPrice(0)
+        else
+            setPrice(parseInt(event.target.value));
     }
     
     const handleCustomPriceChange = (event) => {
         setPrice(parseInt(event.target.value));
+
+        if(event.target.value === '')
+            setPrice(0)
+        else
+            setPrice(parseInt(event.target.value));
     };
 
     return (
@@ -62,25 +78,35 @@ const ServiceForm = () => {
                         <div className='call-field'>
                             <label>Call duration</label>
                             <br />
+
+                            <div className='call-options'>
+
                             <input type="radio" id="10mins" name="duration" value="10" onChange={handleDurationChange} />
-                            <label htmlFor="10mins">10 mins</label>
+                            <label htmlFor="10mins" style={{marginTop:'17px'}}>10 mins</label>
                             <br />
                             <input type="radio" id="20mins" name="duration" value="20" onChange={handleDurationChange} />
-                            <label htmlFor="20mins">20 mins</label>
+                            <label htmlFor="20mins" style={{marginTop:'17px'}}>20 mins</label>
                             <br />
+
                             <input type="radio" id="30mins" name="duration" value="30" onChange={handleDurationChange} />
-                            <label htmlFor="30mins">30 mins</label>
+                            <label htmlFor="30mins" style={{marginTop:'17px'}}>30 mins</label>
                             <br />
                             <input type="radio" id="1hr" name="duration" value="60" onChange={handleDurationChange} />
-                            <label htmlFor="1hr">1 hr</label>
+                            <label htmlFor="1hr" style={{marginTop:'17px'}}>1 hr</label>
                             <br />
+
+                            </div>
+
                             <input type="radio" id="customCall" name="duration" value="custom" onChange={handleDurationChange} />
-                            <label htmlFor="custom">Custom Call</label>
+                            <label htmlFor="custom" style={{marginTop:'17px'}}>Custom Call</label>
+
 
                             {
                                 !customBool &&
                                 <>
-                                    <input type="number" id="customDuration" name="customDuration" min="1" max="1440" value='0' onChange={handleCustomDurationChange} disabled /> mins
+                                    <input type="number" id="customDuration" name="customDuration" min="1" max="1440" value='0' onChange={handleCustomDurationChange} disabled 
+                                    style={{marginLeft:'30px', fontSize: '18px', color: '#C5C6C7', 
+                                            width:'50%'}}/>
                                     <br />
                                 </>
                             }
@@ -88,11 +114,13 @@ const ServiceForm = () => {
                             {
                                 customBool &&
                                 <>
-                                    <input type="number" id="customDuration" name="customDuration" min="1" max="1440" onChange={handleCustomDurationChange} /> mins
+                                    <input type="number" id="customDuration" name="customDuration" min="1" max="1440" onChange={handleCustomDurationChange} 
+                                    placeholder='Enter time in minutes' style={{marginLeft:'30px', fontSize: '18px',
+                                    color: '#C5C6C7', width:'50%'}} />
                                     <br />
                                 </>
                             }
-                            <p>Selected duration: {duration === "customDuration" ? duration + " mins" : duration === 0 ? "" : duration + " mins"}</p>
+                            <p style={{marginTop:'20px' , fontSize:'20px'}}>Selected duration: {customBool ? (duration === 0 ? 'Fix the slot' : duration + " mins") : duration === 0 ? 0 + " mins" : duration + " mins"}</p>
                         </div>
                     </div>
 
@@ -101,25 +129,34 @@ const ServiceForm = () => {
                         <div className='price-field'>
                             <label> Price </label>
                             <br />
+
+                            <div className='call-options'>
+
                             <input type="radio" id="100price" name="price" value="100" onChange={handlePriceChange} />
-                            <label htmlFor="100price">100 Rs</label>
+                            <label htmlFor="100price" style={{marginTop:'17px'}}>100 Rs</label>
                             <br />
                             <input type="radio" id="200price" name="price" value="200" onChange={handlePriceChange} />
-                            <label htmlFor="200price">200 Rs</label>
+                            <label htmlFor="200price" style={{marginTop:'17px'}}>200 Rs</label>
                             <br />
+
                             <input type="radio" id="300price" name="price" value="300" onChange={handlePriceChange} />
-                            <label htmlFor="300price">300 Rs</label>
+                            <label htmlFor="300price" style={{marginTop:'17px'}}>300 Rs</label>
                             <br />
                             <input type="radio" id="500price" name="price" value="500" onChange={handlePriceChange} />
-                            <label htmlFor="500price">500 Rs</label>
+                            <label htmlFor="500price" style={{marginTop:'17px'}}>500 Rs</label>
                             <br />
+                            </div>
+
                             <input type="radio" id="customPrice" name="price" value="custom" onChange={handlePriceChange} />
-                            <label htmlFor="custom">Custom Price</label>
+                            <label htmlFor="custom" style={{marginTop:'17px'}}>Custom Price</label>
 
                             {
                                 !customPriceBool &&
                                 <>
-                                    <input type="number" id="customPrice" name="customPrice" value='0' onChange={handleCustomPriceChange} disabled /> Rs
+                                    <input type="number" id="customPrice" name="customPrice" value='0' onChange={handleCustomPriceChange} disabled 
+                                    style={{marginLeft:'30px', fontSize: '18px',
+                                    color: '#C5C6C7', width:'50%'}}
+                                    />
                                     <br />
                                 </>
                             }
@@ -127,11 +164,13 @@ const ServiceForm = () => {
                             {
                                 customPriceBool &&
                                 <>
-                                    <input type="number" id="customPrice" name="customPrice" onChange={handleCustomPriceChange} /> Rs
+                                    <input type="number" id="customPrice" name="customPrice" onChange={handleCustomPriceChange} 
+                                    placeholder='Enter the amount' style={{marginLeft:'25px', fontSize: '18px',
+                                    color: '#C5C6C7', width:'50%'}} />
                                     <br />
                                 </>
                             }
-                            <p>Selected Price: {duration === "customPrice" ? price + " Rs" : price === 0 ? "" : price + " Rs"}</p>
+                            <p style={{marginTop:'20px' , fontSize:'20px'}}>Selected Price: {customPriceBool ? (price === 0 ? 'Free' : price + " Rs") : price === 0 ? "Free" : price + " Rs"}</p>
                         </div>
                     </div>
 
