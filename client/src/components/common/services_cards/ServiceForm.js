e88083ba04593561cf95987f1ed7d09571cb62f3
Jana <janakrish3011@gmail.com>
2023-05-15 01:19:38 +0530

    Added some css for services

--- client/src/components/common/services_cards/ServiceForm.js
+++ client/src/components/common/services_cards/ServiceForm.js
@@ -21,37 +21,37 @@ const ServiceForm = () => {
         else
             setCustomBool(false);
 
-        if(event.target.value === 'custom')
+        if (event.target.value === 'custom')
             setDuration(0)
-        else 
+        else
             setDuration(parseInt(event.target.value));
     };
 
     const handleCustomDurationChange = (event) => {
         setDuration(parseInt(event.target.value));
 
-        if(event.target.value === '')
+        if (event.target.value === '')
             setDuration(0)
-        else 
+        else
             setDuration(parseInt(event.target.value));
     };
-    
+
     const handlePriceChange = (event) => {
-        if (event.target.id === 'customPrice') 
+        if (event.target.id === 'customPrice')
             setCustomPriceBool(true);
         else
             setCustomPriceBool(false);
-        
-        if(event.target.value === 'custom')
+
+        if (event.target.value === 'custom')
             setPrice(0)
         else
             setPrice(parseInt(event.target.value));
     }
-    
+
     const handleCustomPriceChange = (event) => {
         setPrice(parseInt(event.target.value));
 
-        if(event.target.value === '')
+        if (event.target.value === '')
             setPrice(0)
         else
             setPrice(parseInt(event.target.value));
@@ -81,32 +81,34 @@ const ServiceForm = () => {
 
                             <div className='call-options'>
 
-                            <input type="radio" id="10mins" name="duration" value="10" onChange={handleDurationChange} />
-                            <label htmlFor="10mins" style={{marginTop:'17px'}}>10 mins</label>
-                            <br />
-                            <input type="radio" id="20mins" name="duration" value="20" onChange={handleDurationChange} />
-                            <label htmlFor="20mins" style={{marginTop:'17px'}}>20 mins</label>
-                            <br />
+                                <input type="radio" id="10mins" name="duration" value="10" onChange={handleDurationChange} />
+                                <label htmlFor="10mins" style={{ marginTop: '17px' }}>10 mins</label>
+                                <br />
+                                <input type="radio" id="20mins" name="duration" value="20" onChange={handleDurationChange} />
+                                <label htmlFor="20mins" style={{ marginTop: '17px' }}>20 mins</label>
+                                <br />
 
-                            <input type="radio" id="30mins" name="duration" value="30" onChange={handleDurationChange} />
-                            <label htmlFor="30mins" style={{marginTop:'17px'}}>30 mins</label>
-                            <br />
-                            <input type="radio" id="1hr" name="duration" value="60" onChange={handleDurationChange} />
-                            <label htmlFor="1hr" style={{marginTop:'17px'}}>1 hr</label>
-                            <br />
+                                <input type="radio" id="30mins" name="duration" value="30" onChange={handleDurationChange} />
+                                <label htmlFor="30mins" style={{ marginTop: '17px' }}>30 mins</label>
+                                <br />
+                                <input type="radio" id="1hr" name="duration" value="60" onChange={handleDurationChange} />
+                                <label htmlFor="1hr" style={{ marginTop: '17px' }}>1 hr</label>
+                                <br />
 
                             </div>
 
                             <input type="radio" id="customCall" name="duration" value="custom" onChange={handleDurationChange} />
-                            <label htmlFor="custom" style={{marginTop:'17px'}}>Custom Call</label>
+                            <label htmlFor="custom" style={{ marginTop: '17px' }}>Custom Call</label>
 
 
                             {
                                 !customBool &&
                                 <>
-                                    <input type="number" id="customDuration" name="customDuration" min="1" max="1440" value='0' onChange={handleCustomDurationChange} disabled 
-                                    style={{marginLeft:'30px', fontSize: '18px', color: '#C5C6C7', 
-                                            width:'50%'}}/>
+                                    <input type="number" id="customDuration" name="customDuration" min="1" max="1440" value='0' onChange={handleCustomDurationChange} disabled
+                                        style={{
+                                            marginLeft: '30px', fontSize: '18px', color: '#C5C6C7',
+                                            width: '50%'
+                                        }} />
                                     <br />
                                 </>
                             }
@@ -114,13 +116,15 @@ const ServiceForm = () => {
                             {
                                 customBool &&
                                 <>
-                                    <input type="number" id="customDuration" name="customDuration" min="1" max="1440" onChange={handleCustomDurationChange} 
-                                    placeholder='Enter time in minutes' style={{marginLeft:'30px', fontSize: '18px',
-                                    color: '#C5C6C7', width:'50%'}} />
+                                    <input type="number" id="customDuration" name="customDuration" min="1" max="1440" onChange={handleCustomDurationChange}
+                                        placeholder='Enter time in minutes' style={{
+                                            marginLeft: '30px', fontSize: '18px',
+                                            color: '#C5C6C7', width: '50%'
+                                        }} />
                                     <br />
                                 </>
                             }
-                            <p style={{marginTop:'20px' , fontSize:'20px'}}>Selected duration: {customBool ? (duration === 0 ? 'Fix the slot' : duration + " mins") : duration === 0 ? 0 + " mins" : duration + " mins"}</p>
+                            <p style={{ marginTop: '20px', fontSize: '20px' }}>Selected duration: {customBool ? (duration === 0 ? 'Fix the slot' : duration + " mins") : duration === 0 ? 0 + " mins" : duration + " mins"}</p>
                         </div>
                     </div>
 
@@ -132,30 +136,32 @@ const ServiceForm = () => {
 
                             <div className='call-options'>
 
-                            <input type="radio" id="100price" name="price" value="100" onChange={handlePriceChange} />
-                            <label htmlFor="100price" style={{marginTop:'17px'}}>100 Rs</label>
-                            <br />
-                            <input type="radio" id="200price" name="price" value="200" onChange={handlePriceChange} />
-                            <label htmlFor="200price" style={{marginTop:'17px'}}>200 Rs</label>
-                            <br />
+                                <input type="radio" id="100price" name="price" value="100" onChange={handlePriceChange} />
+                                <label htmlFor="100price" style={{ marginTop: '17px' }}>100 Rs</label>
+                                <br />
+                                <input type="radio" id="200price" name="price" value="200" onChange={handlePriceChange} />
+                                <label htmlFor="200price" style={{ marginTop: '17px' }}>200 Rs</label>
+                                <br />
 
-                            <input type="radio" id="300price" name="price" value="300" onChange={handlePriceChange} />
-                            <label htmlFor="300price" style={{marginTop:'17px'}}>300 Rs</label>
-                            <br />
-                            <input type="radio" id="500price" name="price" value="500" onChange={handlePriceChange} />
-                            <label htmlFor="500price" style={{marginTop:'17px'}}>500 Rs</label>
-                            <br />
+                                <input type="radio" id="300price" name="price" value="300" onChange={handlePriceChange} />
+                                <label htmlFor="300price" style={{ marginTop: '17px' }}>300 Rs</label>
+                                <br />
+                                <input type="radio" id="500price" name="price" value="500" onChange={handlePriceChange} />
+                                <label htmlFor="500price" style={{ marginTop: '17px' }}>500 Rs</label>
+                                <br />
                             </div>
 
                             <input type="radio" id="customPrice" name="price" value="custom" onChange={handlePriceChange} />
-                            <label htmlFor="custom" style={{marginTop:'17px'}}>Custom Price</label>
+                            <label htmlFor="custom" style={{ marginTop: '17px' }}>Custom Price</label>
 
                             {
                                 !customPriceBool &&
                                 <>
-                                    <input type="number" id="customPrice" name="customPrice" value='0' onChange={handleCustomPriceChange} disabled 
-                                    style={{marginLeft:'30px', fontSize: '18px',
-                                    color: '#C5C6C7', width:'50%'}}
+                                    <input type="number" id="customPrice" name="customPrice" value='0' onChange={handleCustomPriceChange} disabled
+                                        style={{
+                                            marginLeft: '30px', fontSize: '18px',
+                                            color: '#C5C6C7', width: '50%'
+                                        }}
                                     />
                                     <br />
                                 </>
@@ -164,15 +170,18 @@ const ServiceForm = () => {
                             {
                                 customPriceBool &&
                                 <>
-                                    <input type="number" id="customPrice" name="customPrice" onChange={handleCustomPriceChange} 
-                                    placeholder='Enter the amount' style={{marginLeft:'25px', fontSize: '18px',
-                                    color: '#C5C6C7', width:'50%'}} />
+                                    <input type="number" id="customPrice" name="customPrice" onChange={handleCustomPriceChange}
+                                        placeholder='Enter the amount' style={{
+                                            marginLeft: '25px', fontSize: '18px',
+                                            color: '#C5C6C7', width: '50%'
+                                        }} />
                                     <br />
                                 </>
                             }
-                            <p style={{marginTop:'20px' , fontSize:'20px'}}>Selected Price: {customPriceBool ? (price === 0 ? 'Free' : price + " Rs") : price === 0 ? "Free" : price + " Rs"}</p>
+                            <p style={{ marginTop: '20px', fontSize: '20px' }}>Selected Price: {customPriceBool ? (price === 0 ? 'Free' : price + " Rs") : price === 0 ? "Free" : price + " Rs"}</p>
                         </div>
                     </div>
+                    <div className='division' style={{ width: '100%', maxWidth: '600px', marginLeft: '10px', marginTop: '20px' }}></div>
 
                     <button className='submit-button' type='submit' onClick={handleSubmit}>Submit</button>
                 </form>
