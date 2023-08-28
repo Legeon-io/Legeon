3ed834143eb7b7b5368e45b6975321862c75162b
Tarun Jaikishan <tarunjaikishanpk@gmail.com>
2023-08-29 00:00:18 +0530

    added bcrypt for backend

--- server/mongodb/models/users.js
+++ server/mongodb/models/users.js
@@ -1,13 +1,26 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const UserSchema = new mongoose.Schema({
-    username: { type: String, required: true, unique: true},
-    firstname: { type: String, required: true},
-    lastname: {type: String, default: 'Last Name'},
-    email: { type: String, required: true, unique: true},
-    password: { type: String, required: true},
+  username: { type: String, required: true, unique: true },
+  firstname: { type: String, required: true },
+  lastname: { type: String, default: "" },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
 });
 
-const userModel = mongoose.model('User', UserSchema);
+UserSchema.pre("save", async function (next) {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(this.password, salt);
+    this.password = hashedPassword;
+    next();
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+});
+
+const userModel = mongoose.model("User", UserSchema);
 
 export default userModel;
