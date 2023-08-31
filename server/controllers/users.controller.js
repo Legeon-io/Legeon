87cd179e913f0537bfa7417a332ae74738a0f724
Tarun Jaikishan <tarunjaikishanpk@gmail.com>
2023-08-31 23:15:22 +0530

    merge complete with LG_014_LoginPart

--- server/controllers/users.controller.js
+++ server/controllers/users.controller.js
@@ -19,9 +19,21 @@ export const signup = async (req, res) => {
     if (!firstname || !email || !password) {
       return res.status(404).json({ error: "Missing Credentials" });
     }
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      const errorMessage = "Email already registered";
+      return res.status(409).json({ errorMessage });
+    }
+    const existingUsername = await User.findOne({ username });
+    if (existingUsername) {
+      username = `${username}#${generateShortUUID()}`;
+    }
+
     const newUser = new User({
       email,
-      username: username + "#" + generateShortUUID(),
+      firstname,
+      username, 
       password,
     });
     await newUser.save();
@@ -37,16 +49,9 @@ export const signup = async (req, res) => {
 export const login = async (req, res) => {
   try {
     const { email, password } = req.body;
-    if (!email || !password) {
-      return res.status(400).json({ error: "Invalid credentials" });
-    }
-    // Check if user exists
     const user = await User.findOne({ email });
-    if (!user) {
-      return res.status(400).json({ error: "Invalid credentials" });
-    }
     const passwordMatch = await bcrypt.compare(password, user.password);
-    if (!passwordMatch) {
+    if (!email || !password || user || passwordMatch) {
       return res.status(400).json({ error: "Invalid credentials" });
     }
     res.status(200).json({ message: "Login successful", user: user });
@@ -54,6 +59,8 @@ export const login = async (req, res) => {
     res.status(500).json({ error: "Internal server error", error });
   }
 };
+
+
 
 // Get User function
 export const getUser = async (req, res) => {
@@ -64,7 +71,6 @@ export const getUser = async (req, res) => {
     if (!user) {
       return res.status(401).json({ error: "User not registered" });
     }
-
     res.status(200).json({ message: "User information received", user: user });
   } catch (error) {
     res.status(500).json({ error: "Internal server error", error });
