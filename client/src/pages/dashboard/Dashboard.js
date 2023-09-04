12d250d69de6226f22b139b5553ff5b391d243dc
hsv123456789 <harisrevatcha@gmail.com>
2023-09-04 19:27:39 +0530

    made some minor changes

--- client/src/pages/dashboard/Dashboard.js
+++ client/src/pages/dashboard/Dashboard.js
@@ -14,27 +14,27 @@ const Dashboard = ({ sidebarVisible }) => {
     <>
       {/* First div   */}
       <div id="maindiv" className="w-full">
-        <div className="flex flex-col md:flex-row gap-10 md:m-16 m-8 mt-10 mb-0">
+        <div className="flex flex-col lg:flex-row gap-10 lg:m-16 m-8 mt-10 mb-0">
           <img
             alt="workers working together"
             src={image1}
-            className="w-full md:w-1/2"
+            className="w-full lg:w-1/2"
           />
           <div className="flex justify-center items-center flex-col gap-10 ">
-            <div className=" hidden md:flex justify-start ">
-              <h1 className=" text-2xl  md:text-9xl bg-gradient-to-r to-pink-500 from-indigo-500 via-purple-500 text-transparent bg-clip-text font-semibold ">
+            <div className=" hidden lg:flex justify-start ">
+              <h1 className=" text-2xl  lg:text-9xl bg-gradient-to-r to-pink-500 from-indigo-500 via-purple-500 text-transparent bg-clip-text font-semibold ">
                 LEGE
               </h1>
               <img
                 src={logo}
-                className="rounded-full sm:h-10 md:h-32 shadow-sm shadow-black"
+                className="rounded-full sm:h-10 lg:h-32 shadow-sm shadow-black"
                 alt="logo of legeon"
               />
-              <h1 className=" text-2xl md:text-9xl bg-gradient-to-r to-pink-500 from-indigo-500 via-purple-500 text-transparent bg-clip-text font-semibold">
+              <h1 className=" text-2xl lg:text-9xl bg-gradient-to-r to-pink-500 from-indigo-500 via-purple-500 text-transparent bg-clip-text font-semibold">
                 N
               </h1>
             </div>
-            <div className="text-xl md:text-3xl font-semibold text-gray-800">
+            <div className="text-xl lg:text-3xl font-semibold text-gray-800">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua.
             </div>
@@ -46,16 +46,16 @@ const Dashboard = ({ sidebarVisible }) => {
       </div>
 
       {/** second div */}
-      <div className="bg-custom-blue h-full w-full p-10 md:pt-0 md:-translate-y-20 translate-y-0 ">
-        <div className="flex flex-col md:flex-row gap-10">
+      <div className="bg-custom-blue h-full w-full p-10 lg:pt-0 lg:-translate-y-20 translate-y-0 ">
+        <div className="flex flex-col lg:flex-row gap-10">
           <div className="flex flex-col gap-10 mt-4">
             <div>
               <h1 className="text-6xl text-white border-l-[0.5rem] pl-4 border-l-white">
                 Revenue Stream
               </h1>
             </div>
-            <div className="flex flex-col md:flex-row justify-between gap-10">
-              <p className="text-xl md:text-2xl text-justify text-white">
+            <div className="flex flex-col lg:flex-row justify-between gap-10">
+              <p className="text-xl lg:text-2xl text-justify text-white">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
                 eiusmod tempor incididunt ut labore et dolore magna aliqua lorem
                 ipsum dolor sit amet, consectetur adipiscing elit. Sed do
@@ -74,8 +74,8 @@ const Dashboard = ({ sidebarVisible }) => {
       </div>
 
       {/* third div*/}
-      <div className="flex flex-col md:flex-row m-10 mt-20 md:m-20 gap-20 md:gap-0 justify-between">
-        <div className="bg-gray-200 flex flex-col w-full md:w-1/4 rounded-xl p-10">
+      <div className="flex flex-col lg:flex-row m-10 mt-20 lg:m-20 gap-20 lg:gap-0 justify-between">
+        <div className="bg-gray-200 flex flex-col w-full lg:w-1/4 rounded-xl p-10">
           <img src={communication} alt="people communicating with each other" />
           <h1 className="text-4xl">Lorem ipsum</h1>
           <p className="text-start">
@@ -90,19 +90,19 @@ const Dashboard = ({ sidebarVisible }) => {
           </div>
         </div>
 
-        <div className="flex justify-center items-center ml-10 mt-10 md:mt-0">
-          <div className="bg-gradient-to-r to-pink-500 from-indigo-500 via-purple-500 text-transparent bg-clip-text font-semibold text-4xl md:text-8xl">
+        <div className="flex justify-center items-center ml-10 mt-10 lg:mt-0">
+          <div className="bg-gradient-to-r to-pink-500 from-indigo-500 via-purple-500 text-transparent bg-clip-text font-semibold text-4xl lg:text-8xl">
             Make robust <br /> connections
           </div>
         </div>
       </div>
 
       {/**fourth div */}
-      <div className="flex flex-col m-10 gap-20 mt-20 md:m-20 md:gap-0 justify-between md:flex-row-reverse">
-        <div className="bg-gray-200 flex flex-col w-full md:w-1/4 rounded-xl p-10">
+      <div className="flex flex-col m-10 gap-20 mt-20 lg:m-20 lg:gap-0 justify-between lg:flex-row-reverse">
+        <div className="bg-gray-200 flex flex-col w-full lg:w-1/4 rounded-xl p-10">
           <img src={handshake} alt="people communicating with each other" />
           <h1 className="text-2xl">Lorem ipsum</h1>
-          <p className="md:text-start  ">
+          <p className="lg:text-start  ">
             consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut
             labore et dolore magna aliqua lorem ipsum dolor sit amet,
             consectetur
@@ -114,8 +114,8 @@ const Dashboard = ({ sidebarVisible }) => {
           </div>
         </div>
 
-        <div className="flex justify-center items-center ml-10 mt-10 md:mt-0">
-          <div className="bg-gradient-to-r to-pink-500 from-indigo-500 via-purple-500 text-transparent bg-clip-text font-semibold text-4xl md:text-8xl">
+        <div className="flex justify-center items-center ml-10 mt-10 lg:mt-0">
+          <div className="bg-gradient-to-r to-pink-500 from-indigo-500 via-purple-500 text-transparent bg-clip-text font-semibold text-4xl lg:text-8xl">
             Be the mentor <br /> and the mentee <br /> at the same time
           </div>
         </div>
@@ -123,10 +123,10 @@ const Dashboard = ({ sidebarVisible }) => {
 
       {/*fifth div */}
       <div className="bg-custom-orange h-full w-full p-10">
-        <div className="flex flex-col md:flex-row-reverse gap-10">
+        <div className="flex flex-col lg:flex-row-reverse gap-10">
           <img
             src={divthreeimage}
-            className="shadow-xl rounded-2xl w-full md:w-1/2"
+            className="shadow-xl rounded-2xl w-full lg:w-1/2"
             alt="girl working with calendar"
           />
 
@@ -136,7 +136,7 @@ const Dashboard = ({ sidebarVisible }) => {
                 Your Network Is The Network You Build
               </h1>
             </div>
-            <p className="text-lg  md:text-2xl text-white text-justify">
+            <p className="text-lg  lg:text-2xl text-white text-justify">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua lorem
               ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod
