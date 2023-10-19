658f979e14ffa8b9ed93e67005ba803afa107e8f
Tarun Jaikishan <tarunjaikishanpk@gmail.com>
2023-10-19 23:12:38 +0530

    changes on class for services

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,18 @@
 import React, { useEffect, useState } from "react";
-import {
-  BrowserRouter,
-  Route,
-  Routes,
-  Navigate,
-  useLocation,
-} from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 
 import Sidebar from "./components/layout/sidebar/Sidebar";
 import {
   LandingPage,
   Services,
-  Bookings,
-  Earnings,
   Profile,
-  Community,
   About,
   Support,
   Feedback,
-  Payments,
 } from "./pages/index.js";
 
 import "./App.css";
 
-import {
-  CreateService,
-  EngageCall,
-  TextQuery,
-} from "../src/pages/services/createService/index.js";
-import EditCallServiceForm from "./components/common/services_cards/EditCallServiceForm.js";
-import UserServices from "./pages/services/user_services/UserServices.js";
-import BookingService from "./pages/services/user_services/BookingService.js";
 import ProtectedRoute from "./ProtectedRoute.js";
 import Dashboard from "./pages/dashboard/Dashboard.js";
 
@@ -94,24 +76,10 @@ const App = () => {
                 <Route path="/dashboard" element={<Dashboard />} />
                 <Route path="/services" element={<Services />} />
                 <Route path="/profile" element={<Profile />} />
-                {/* <Route path="/bookings" element={<Bookings />} /> */}
-                {/* <Route path="/payments" element={<Payments />} /> */}
-                {/* <Route path="/earnings" element={<Earnings />} /> */}
-                {/* <Route path="/profile" element={<Profile />} /> */}
-                {/* <Route path="/account" element={<AccountPage />} /> */}
-                {/* <Route path="/community" element={<Community />} /> */}
+
                 <Route path="/about" element={<About />} />
                 <Route path="/support" element={<Support />} />
                 <Route path="/feedback" element={<Feedback />} />
-                <Route
-                  path="/services/create-service"
-                  element={<CreateService />}
-                />
-                {/* <Route path="/services/create-service/engage-call" element={<EngageCall />} /> */}
-                {/* <Route path="/services/create-service/text-query" element={<TextQuery />} /> */}
-                {/* <Route path="/services/engage-call/edit-service/:id" element={<EditCallServiceForm />} /> */}
-                {/* <Route path="/:username" element={<UserServices />} /> */}
-                {/* <Route path="/:username/:title/:id/service" element={<BookingService />} /> */}
               </Route>
               <Route path="/:username" element={<ServiceHub />} />
               <Route path="/404" element={<NotFound />} />
--- client/src/pages/services/Services.js
+++ client/src/pages/services/Services.js
@@ -7,9 +7,9 @@ import PlaceHolderServices from "./PlaceHolderServices";
 import CreateService from "./createService/CreateService";
 import CreateMessageService from "./createService/CreateMessageService";
 import { ServiceList } from "./ServiceList";
-import ServiceCards from "../../components/common/ServiceCards";
 import Cookies from "js-cookie";
 import axios from "axios";
+import { Route, Routes } from "react-router-dom";
 export const Services = ({ sidebarVisible }) => {
   const [data, setData] = useState([]);
   //To get all routes
@@ -31,7 +31,6 @@ export const Services = ({ sidebarVisible }) => {
     })();
   }, []);
 
-  const loading = false;
   const [pageState, setPageState] = useState(0);
   const [service, createService] = useState(false);
   const [serviceEmpty, setServiceEmpty] = useState(false);
@@ -71,69 +70,53 @@ export const Services = ({ sidebarVisible }) => {
 
   return (
     <>
-      {loading ? (
-        <div class="md:grid xl:grid-cols-3 md:grid-cols-2 items-center gap-10 w-full p-10">
-          <div className="h-[20rem] w-full bg-gray-400 rounded-3xl animate-pulse" />
-          <div className="hidden md:block h-[20rem] w-full bg-gray-400 rounded-3xl animate-pulse" />
-          <div className="hidden xl:block h-[20rem] w-full bg-gray-400 rounded-3xl animate-pulse" />
-          <span class="flex w-full items-center justify-center col-span-3 text-5xl text-gray-400 animate-pulse">
-            Loading
-            <p className="animate-bounce">.</p>
-            <p className="animate-bounce">.</p>
-            <p className="animate-bounce">.</p>
-          </span>
-        </div>
-      ) : (
-        <div>
-          <div className="grid grid-cols-7 h-screen">
-            <div className="col-span-6 grid grid-rows-6">
-              <div className="flex flex-col w-full">
-                <h1 className="text-3xl mt-5 mx-5 md:ml-10">Services</h1>
-                <div className="flex flex-row items-center gap-x-4 md:justify-between mx-5 md:mx-10 mt-3">
-                  <div className="flex flex-row justify-between w-full">
-                    <div className="flex flex-row gap-4 md:gap1-10 mt-3">
-                      <button
-                        className={`border-2 border-black md:px-10 p-2 text-center rounded-3xl ${
-                          pageState === 0
-                            ? "bg-gradient-to-r to-pink-500 from-indigo-500 via-purple-500 text-white border-none"
-                            : ""
-                        } `}
-                        onClick={() => {
-                          buttonSubmit(0);
-                        }}
-                      >
-                        1:1 services
-                      </button>
-                      <button
-                        className={`border-2 border-black md:px-10 p-2 text-center rounded-3xl ${
-                          pageState === 1
-                            ? "bg-gradient-to-r to-pink-500 from-indigo-500 via-purple-500 text-white border-none"
-                            : ""
-                        } `}
-                        to="/profile/editProfile"
-                        onClick={() => {
-                          buttonSubmit(1);
-                        }}
-                      >
-                        Messages
-                      </button>
-                    </div>
-                    <button
-                      className=" px-1 py-2   border-black border-2 rounded-md"
-                      onClick={() => {
-                        buttonSubmit(2);
-                      }}
-                    >
-                      create Services
-                    </button>
-                  </div>
+      <div>
+        <div className="h-screen">
+          <div className="flex flex-col w-full">
+            <h1 className="text-3xl mt-5 mx-5 md:ml-10">Services</h1>
+            <div className="flex flex-row items-center gap-x-4 md:justify-between mx-5 md:mx-10 mt-3">
+              <div className="flex flex-row justify-between w-full">
+                <div className="flex flex-row gap-4 md:gap1-10 mt-3">
+                  <button
+                    className={`border-2 border-black md:px-10 p-2 text-center rounded-3xl ${
+                      pageState === 0
+                        ? "bg-gradient-to-r to-pink-500 from-indigo-500 via-purple-500 text-white border-none"
+                        : ""
+                    } `}
+                    onClick={() => {
+                      buttonSubmit(0);
+                    }}
+                  >
+                    1:1 services
+                  </button>
+                  <button
+                    className={`border-2 border-black md:px-10 p-2 text-center rounded-3xl ${
+                      pageState === 1
+                        ? "bg-gradient-to-r to-pink-500 from-indigo-500 via-purple-500 text-white border-none"
+                        : ""
+                    } `}
+                    to="/profile/editProfile"
+                    onClick={() => {
+                      buttonSubmit(1);
+                    }}
+                  >
+                    Messages
+                  </button>
                 </div>
+                <button
+                  className=" px-1 py-2   border-black border-2 rounded-md"
+                  onClick={() => {
+                    buttonSubmit(2);
+                  }}
+                >
+                  create Services
+                </button>
               </div>
-              {content}
             </div>
           </div>
+          {content}
         </div>
-      )}
+      </div>
     </>
   );
 };
