d6c92700e9c5c713a1851aaaa939bba970154101
Janarthanan K <53388646+Janakrish1@users.noreply.github.com>
2023-10-31 22:25:55 +0530

    Revert "Tarun branch updated is merged with develop_backup"

--- server/models/callservices.js
+++ server/models/callservices.js
@@ -1,13 +1,13 @@
 import mongoose from "mongoose";
 
 const CallServicesSchema = new mongoose.Schema({
-  username: { type: String, required: true },
-  servicetype: { type: String, required: true },
-  title: { type: String, required: true },
-  duration: { type: Number, required: true },
-  price: { type: Number },
+    username: { type: String, required: true},
+    servicetype: {type: String, required: true},
+    title: {type: String, required: true},
+    duration: {type: Number, required: true},
+    price: { type: Number},
 });
 
-const callServicesModel = mongoose.model("CallServices", CallServicesSchema);
+const callServicesModel = mongoose.model('CallServices', CallServicesSchema);
 
 export default callServicesModel;
