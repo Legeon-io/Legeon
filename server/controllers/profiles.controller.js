8b8eaf5ee2bcd92318f0bf12350619798fb14a9d
ShailendraTrivedi <shailendratrivedi009@gmail.com>
2023-11-15 15:23:00 +0530

    google-changes

--- server/controllers/profiles.controller.js
+++ server/controllers/profiles.controller.js
@@ -1,5 +1,5 @@
 import user from "../models/users.js";
-import googleUser from "../models/googleuser.js";
+import googleUser from "../models/googleUser.js";
 import profile from "../models/profiles.js";
 import bcrypt from "bcrypt";
 import mongoose from "mongoose";
